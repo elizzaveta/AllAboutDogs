bfc66ba5c65b489d0bafc4383aaf6dd6bae3d922
elizzaveta <y.timchenko-2025@kpi.com>
2022-11-15 21:10:33 +0200

    Made header hide/reveal on scroll

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,49 +1,65 @@
 import React from 'react';
-import {AppBar, Box, Button, Container, ThemeProvider, Toolbar, Typography} from "@material-ui/core";
+import {
+    AppBar,
+    Box,
+    Container,
+    Slide,
+    ThemeProvider,
+    Typography,
+    useScrollTrigger
+} from "@material-ui/core";
 import {theme} from "../styles/Theme";
 import {Link} from "react-router-dom";
+import styles from "../styles/css/components/Header.module.css"
 
-// may be improved? + add adaptivity
 const Header = () => {
     return (
         <ThemeProvider theme={theme}>
-            <AppBar color='primary' elevation={0}>
-                <Container>
-                    <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', padding: '10px'}}>
-                        <Box sx={{display: 'flex', justifyContent: 'space-between', width: '50%'}}>
-                            <Typography variant='h2'>
-                                <Link to='/'>All About Dogs</Link>
-                            </Typography>
-                            <Box sx={{
-                                display: 'flex',
-                                alignItems: 'center',
-                                width: '50%',
-                                justifyContent: 'space-between'
-                            }}>
-                                <Typography variant='h3'>
-                                    <Link to='/breeds'>Breeds</Link>
+            <HideOnScroll>
+                <AppBar color='primary' elevation={0}>
+                    <Container>
+                        <Box className={styles.wrapper}>
+                            <Box className={styles.leftContentWrapper}>
+                                <Typography variant='h2'>
+                                    <Link to='/'>All About Dogs</Link>
                                 </Typography>
+                                <Box className={styles.navigationItemsWrapper}>
+                                    <Typography variant='h3'>
+                                        <Link to='/breeds'>Breeds</Link>
+                                    </Typography>
+                                    <Typography variant='h3'>
+                                        <Link to='/blog'>Blog</Link>
+                                    </Typography>
+                                    <Typography variant='h3'>
+                                        <Link to='/about'>About</Link>
+                                    </Typography>
+                                </Box>
+                            </Box>
+                            <Box>
                                 <Typography variant='h3'>
-                                    <Link to='/blog'>Blog</Link>
-                                </Typography>
-                                <Typography variant='h3'>
-                                    <Link to='/about'>About</Link>
+                                    <Link to='/login'>Log in</Link>
                                 </Typography>
                             </Box>
                         </Box>
-                        <Box>
-                            <Typography variant='h3'>
-                                <Link to='/login'>Log in</Link>
-                            </Typography>
-                        </Box>
-                    </Box>
 
-                </Container>
-            </AppBar>
-
+                    </Container>
+                </AppBar>
+            </HideOnScroll>
         </ThemeProvider>
 
-    );
+    )
+        ;
 };
+
+function HideOnScroll(props) {
+    const {children} = props;
+    const trigger = useScrollTrigger();
+
+    return (
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
+    );
+}
 
 export default Header;
